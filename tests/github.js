3533d553a9b5fac0d91335adc29666a52ab9b1fe
Lei Zhu <lzhu@walmart.com>
2016-03-18 16:40:54 -0700

    getElValue works

--- tests/github.js
+++ tests/github.js
@@ -9,7 +9,13 @@ module.exports = new Test({
 
   "Navigate to magellan-nightwatch repo": function (client) {
     client
-      .getEl("#lst-ib")
+      .getEl("#lst-ib:visible")
+      .getEls("#lst-ib:visible", function(els){
+        console.log(els);
+      })
+      .getElValue("#prm", function(value){
+        console.log(value)
+      })
   }
 
 });

ef6b9e8ccb63af127b44c2de493a9446a84fb4a6
Lei Zhu <lzhu@walmart.com>
2016-03-21 11:51:35 -0700

    command demo

--- tests/github.js
+++ tests/github.js
@@ -10,12 +10,15 @@ module.exports = new Test({
   "Navigate to magellan-nightwatch repo": function (client) {
     client
       .getEl("#lst-ib:visible")
-      .getEls("#lst-ib:visible", function(els){
+      .getEls("#lst-ib:visible", function (els) {
         console.log(els);
       })
-      .getElValue("#prm", function(value){
+      .clickEl("a[href='//www.google.com/intl/en/about.html?fg=1']")
+      .getElValue("#corp-crumb", function (value) {
         console.log(value)
       })
+      .assert.elContainsText("#corp-crumb", "About Google")
+      .assert.selectorHasLength("#corp-crumb", 1);
   }
 
 });

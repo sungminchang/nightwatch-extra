75c66b11519277ab25369f770cae84efdbedaf7b
Lei Zhu <thunderzhulei@gmail.com>
2017-02-02 17:44:54 -0800

    add cmd support for native app test

---
+++ src/commands/mobile/setMobileElValue.js
@@ -0,0 +1,52 @@
+import util from "util";
+import BaseCommand from "../../base-mobile-command";
+
+const SetMobileElValue = function (nightwatch = null) {
+  BaseCommand.call(this, nightwatch);
+  this.cmd = "setmobileelvalue";
+};
+
+util.inherits(SetMobileElValue, BaseCommand);
+
+SetMobileElValue.prototype.do = function (value) {
+  const self = this;
+
+  this.protocol(
+    {
+      path: `/session/${this.client.sessionId}/element/${value.ELEMENT}/value`,
+      method: "POST",
+      data: {
+        value: this.valueToSet
+      }
+    },
+    (result) => {
+      if (result.status === 0) {
+        self.pass(result.value);
+      } else {
+        self.fail();
+      }
+    });
+};
+
+/*eslint max-params:["error", 4] */
+SetMobileElValue.prototype.command = function (using, selector, valueToSet, cb) {
+  this.selector = selector;
+  this.using = using;
+  this.valueToSet = valueToSet;
+
+  this.cb = cb;
+
+  this.successMessage = `Selector '${this.using}:${this.selector}' `
+    + `set value to '${this.valueToSet}' after %d milliseconds.`;
+  this.failureMessage = `Selector '${this.using}:${this.selector}' `
+    + `can't set value to '${this.valueToSet}' after %d milliseconds.`;
+
+  this.startTime = (new Date()).getTime();
+
+  this.seenCount = 0;
+  this.checkConditions();
+
+  return this;
+};
+
+module.exports = SetMobileElValue;
